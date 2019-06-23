23b29d34208bfc955fb3f22930a3bbd717ea0bfb
Kian Davoudi-Rad <kian@Kians-MacBook-Pro.local>
2019-06-23 08:10:05 -0700

    Directory added nodejs

--- Tutorial/Node_Ess2/Ch05/05_04/start/directory.js
+++ Tutorial/Node_Ess2/Ch05/05_04/start/directory.js
@@ -1,13 +1,13 @@
-var fs = require ('fs');
+var fs = require('fs');
 
 if (fs.existsSync('lib')){
 console.log('Directory already there');
 }else {
-
-    fs.mkdir('lib',function(err){
-        if (err){
-            console.log(err);
-        }
-        console.log('directory created');
-    });
+fs.mkdir('lib', function(err){
+    if (err){
+        console.log(err);
+    }else {
+        console.log('Directory Created');
+    }
+});
 }
