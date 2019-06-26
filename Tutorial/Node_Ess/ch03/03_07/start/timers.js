db88b265e0f4e165fc4b7deefd2bffd95e6d3e2d
Kian Davoudi-Rad <kian@Kians-MacBook-Pro.local>
2019-06-26 15:08:34 -0700

    added timers

--- Tutorial/Node_Ess/ch03/03_07/start/timers.js
+++ Tutorial/Node_Ess/ch03/03_07/start/timers.js
@@ -1,6 +1,17 @@
 const waitTime = 3000;
+
+const waiteInterval = 500;
+let currentTime = 0;
+const incTime = ()=>{
+    currentTime += waiteInterval;
+    const p = Math.floor((currentTime/waitTime) + 100);
+};
 console.log(`setting a ${waitTime / 1000} second delay`);
 
-const timerFinished = () => console.log("done");
+const timerFinished = () => {
+clearInterval(interval);
+    console.log("done");
+};
 
+const interval = setInterval(incTime, waiteInterval);
 setTimeout(timerFinished, waitTime);
