89f2477192fe0f99a3cfb37f5baacf3a533523c1
Kian Davoudi-Rad <kian@Kians-MacBook-Pro.local>
2019-06-25 19:58:37 -0700

    Adding new practice ran successfully

---
+++ Tutorial/Node_Ess2/Ch09/09_04/app.js
@@ -0,0 +1,20 @@
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: 3000});
+
+// while connected to server instance
+wss.on('connection', function(ws){
+
+    ws.on('message', function(message){
+
+        if (message === 'exit'){
+            ws.close();
+        }
+        // broadcast message
+        wss.clients.forEach(function(client){
+            client.send(message);
+        });
+    });
+
+    // Initial event
+    ws.send('Welcome to chat app');
+});
