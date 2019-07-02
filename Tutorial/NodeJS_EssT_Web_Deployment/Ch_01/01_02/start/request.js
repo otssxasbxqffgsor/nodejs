581a111aea42897d4276e8181e3fbd88149e3cce
Kian Davoudi-Rad <kian@bk11d8ec.digital-dispatch.com>
2019-07-02 14:44:59 -0700

    adding get http

--- Tutorial/NodeJS_EssT_Web_Deployment/Ch_01/01_02/start/request.js
+++ Tutorial/NodeJS_EssT_Web_Deployment/Ch_01/01_02/start/request.js
@@ -1,6 +1,19 @@
 const https = require("https");
 const fs = require("fs");
 
+const request = https.get(
+  "https://en.wikipedia.org/wiki/Charlie_Brown",
+  res=>{
+    let download = fs.createWriteStream('Charlie_Brown.html');
+    res.pipe(download);
+    res.on('end',()=>{
+      console.log("Response Finished: Wiki page downloaded");
+    });
+  }
+);
+
+request.end();
+/*
 const options = {
   hostname: "en.wikipedia.org",
   port: 443,
@@ -28,3 +41,4 @@ const request = https.request(options, res => {
 });
 
 request.end();
+*/
