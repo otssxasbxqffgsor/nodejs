cd914dc37c0b1d7f8e44536b01e0886a7ddcac57
Kian Davoudi-Rad <kian@yvr-tx-miodrag.digital-dispatch.com>
2019-07-02 15:29:37 -0700

    adding practice

--- Tutorial/NodeJS_EssT_Web_Deployment/Ch_01/01_05/start/server.js
+++ Tutorial/NodeJS_EssT_Web_Deployment/Ch_01/01_05/start/server.js
@@ -1,5 +1,6 @@
 const { createServer } = require("http");
 const { createReadStream } = require("fs");
+const { decode } = require("querystring");
 
 const sendFile = (res, status, type, filePath) => {
   res.writeHead(status, { "Content-Type": type });
@@ -7,9 +8,22 @@ const sendFile = (res, status, type, filePath) => {
 };
 
 createServer((req, res) => {
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", data => {
+      body += data;
+    });
+    req.on("end", () => {
+      const { name, email, message } = decode(body);
+      console.log(`${name} (${email}): ${message}`);
+    });
+  }
+
   switch (req.url) {
     case "/":
       return sendFile(res, 200, "text/html", "./home-page.html");
+    case "/contact":
+      return sendFile(res, 200, "text/html", "./contact.html");
     case "/img/alex-banks.jpeg":
       return sendFile(res, 200, "image/jpeg", "./alex-banks.jpeg");
     case "/styles.css":
