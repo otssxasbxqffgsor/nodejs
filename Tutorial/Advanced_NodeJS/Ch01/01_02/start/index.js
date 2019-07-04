28efa00139fb85df73b655bfc7962f61930a8d00
Kian Davoudi-Rad <kian@yvr-tx-miodrag.digital-dispatch.com>
2019-07-04 11:06:38 -0700

    adding resolves and then from Promise method practice

--- Tutorial/Advanced_NodeJS/Ch01/01_02/start/index.js
+++ Tutorial/Advanced_NodeJS/Ch01/01_02/start/index.js
@@ -1,9 +1,12 @@
-function delay(seconds, callback) {
-    setTimeout(callback, seconds*1000);
-}
+var delay = (seconds) => new Promise((resolves, rejects) => {
+    setTimeout(() => {
+        resolves('the long delay has ended')
+    }, seconds*1000);
+});
 
-delay(1, () => {
-    console.log('one second');
-})
+delay(3)
+  .then(console.log)
+  .then(() => 10)
+  .then((number) => console.log(`Hello world: ${number}`))
 
 console.log('end first tick');
