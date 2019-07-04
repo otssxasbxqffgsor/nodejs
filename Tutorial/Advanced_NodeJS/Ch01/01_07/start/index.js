673378ca9faafce3257d596a58eb02b14e1a0b82
Kian Davoudi-Rad <kian@yvr-st-davidb.digital-dispatch.com>
2019-07-04 12:07:55 -0700

    Added latest practice

--- Tutorial/Advanced_NodeJS/Ch01/01_07/start/index.js
+++ Tutorial/Advanced_NodeJS/Ch01/01_07/start/index.js
@@ -7,3 +7,11 @@ var beep = () => process.stdout.write("\x07");
 var delay = (seconds) => new Promise((resolves) => {
     setTimeout(resolves, seconds*1000);
 })
+
+Promise.race([
+    delay(5),
+    delay(2),
+    delay(3),
+    delay(5)
+  ]).then(() => readdir(__dirname))
+    .then(console.log);
