0056ee8d8b8cf752a530c905d8b82c9c71b68209
Kian Davoudi-Rad <kian@Kians-MacBook-Pro.local>
2019-07-07 15:14:38 -0700

    adding paromise queue practice

--- Tutorial/NodeJsAdvanced/Ch01/01_07/start/index.js
+++ Tutorial/NodeJsAdvanced/Ch01/01_07/start/index.js
@@ -5,5 +5,15 @@ var unlink = promisify(fs.unlink);
 var readdir = promisify(fs.readdir);
 var beep = () => process.stdout.write("\x07");
 var delay = (seconds) => new Promise((resolves) => {
+    console.log(`delay for ${seconds*1000} `);
     setTimeout(resolves, seconds*1000);
-})
+});
+
+Promise.race([
+    delay(5).then(()=>console.log('end 5')),
+    delay(2).then(()=>console.log('end 2')),
+    delay(3).then(()=>console.log('end 3')),
+    delay(4).then(()=>console.log('end 4'))
+  ]).then(() => readdir(__dirname))
+    .then(console.log);
+  
--- Tutorial/NodeJsAdvanced/Ch01/01_09/start/index.js
+++ Tutorial/NodeJsAdvanced/Ch01/01_09/start/index.js
@@ -1,3 +1,6 @@
+// var logUpdate = require('log-update');
+// var toX = () => 'X';
+
 var delay = (seconds) => new Promise((resolves) => {
     setTimeout(resolves, seconds*1000);
 });
@@ -12,5 +15,43 @@ var tasks = [
   delay(9),
   delay(10),
   delay(3),
-  delay(5)
+  delay(5),
 ];
+
+class PromiseQueue{
+  constructor(promises = [], currentTasksCount = 1){
+    this.currentTasks = currentTasksCount;
+    this.total = promises.length;
+    this.todoList = promises;
+    this.runningList = [];
+    this.completeList = [];
+  }
+  get runAnother (){
+    return (this.runningList < this.currentTasks && this.todoList.length);
+  }
+  graphTaks(){
+    var {todoList, runningList, completeList } = this;
+    console.log(
+      `
+      toDoList: [${todoList.length}]
+      runningList: [${runningList.length}]
+      completeList: [${completeList.length}]
+      `
+    );
+  }
+  run(){
+    while (this.runAnother){
+      var promise = this.todoList.shift();
+      promise.then(()=>{ 
+        this.completeList.push(this.runningList.shift());
+        this.graphTaks();
+        this.run();
+      });
+      this.runningList.push(promise);
+      this.graphTaks();
+    }
+  }
+}
+
+var delayQueue = new PromiseQueue(tasks, 2);
+delayQueue.run();
