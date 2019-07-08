67cb4f2f98293dfa876e236d1f23a486ba06dfd5
Kian Davoudi-Rad <kian@yvr-aparissay.digital-dispatch.com>
2019-07-08 14:33:21 -0700

    Adding the streamer practice

--- Tutorial/NodeJsAdvanced/Ch03/03_02/start/index.js
+++ Tutorial/NodeJsAdvanced/Ch03/03_02/start/index.js
@@ -1,14 +1,37 @@
 const { createServer } = require('http');
-const { stat, createReadStream } = require('fs');
+const { stat, createReadStream } = require ('fs');
 const { promisify } = require('util');
 const fileName = '../../powder-day.mp4';
-const fileInfo = promisify(stat);
+const fileInfo = promisify (stat);
 
 createServer(async (req, res) => {
   const { size } = await fileInfo(fileName);
-  res.writeHead(200, {
-    'Content-Length': size,
-    'Content-Type': 'video/mp4'
-  });
-  createReadStream(fileName).pipe(res);
+  const range = req.headers.range;
+  if (range) {
+    let [start, end] = range.replace(/bytes=/, '').split('-');
+    start = parseInt(start, 10);
+    end = end ? parseInt(end, 10) : size - 1;
+    res.writeHead(206, {
+       'Content-Range': `bytes ${start}-${end}/${size}`,
+       'Accept-Ranges': 'bytes',
+       'Content-Length': (end-start) + 1,
+       'Content-Type': 'video/mp4'
+    })
+    createReadStream(fileName, { start, end }).pipe(res);
+  } else {
+    res.writeHead(200, {
+      'Content-Length': size,
+      'Content-Type': 'video/mp4'
+    });
+    createReadStream(fileName).pipe(res);
+  }
 }).listen(3000, () => console.log('server running - 3000'));
+
+
+// Killing the process. 
+process.stdout.on('data',(data)=>{
+  if (data.toString().trim() === "exit"){
+      process.exit(1);
+  }
+  console.log (data.toString().trim());
+});
