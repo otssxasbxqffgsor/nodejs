0a7a7199b8c72164d8b6bb5ed39c3713d0850648
Kian Davoudi-Rad <kian@yvr-st-davidb.digital-dispatch.com>
2019-07-11 12:13:15 -0700

    adding prototype practice

--- Tutorial/Node_Design_Patterns/Ch02/02_05/start/index.js
+++ Tutorial/Node_Design_Patterns/Ch02/02_05/start/index.js
@@ -1,12 +1,12 @@
 var scout_prototype = require('./scout_prototype');
 
 var alex = scout_prototype.clone();
-alex.name = 'Alex Banks';
+alex.name = 'alex bank';
 alex.addItemToList('slingshot');
 
 var eve = scout_prototype.clone();
-eve.name = 'Eve Porcello';
+eve.name = 'Eve Procello';
 eve.addItemToList('reading light');
 
-console.log( `${alex.name}: ${alex.shoppingList}` );
-console.log( `${eve.name}: ${eve.shoppingList}` );
+console.log(`${alex.name}: ${alex.shoppingList}`);
+console.log(`${eve.name}: ${eve.shoppingList}`);
