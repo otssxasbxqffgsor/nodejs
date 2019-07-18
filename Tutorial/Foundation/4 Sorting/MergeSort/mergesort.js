91b8f172a8cd5b1499d47159e4dbc80b52a83d77
kian <kian@yvr-st-ladya.digital-dispatch.com>
2019-07-18 10:38:41 -0700

    cleaning up code

--- Tutorial/Foundation/4 Sorting/MergeSort/mergesort.js
+++ Tutorial/Foundation/4 Sorting/MergeSort/mergesort.js
@@ -2,12 +2,12 @@ const mergeSort = (data = []) =>{
 
     // TODO: Stop condition
     while (data.length >1 && data !== 'undefied'){
+    
     // TODO: Select left and right array
     let midd = parseInt(data.length / 2); console.log(`midd is ${midd}`);
     let rD = data.slice(midd, data.length);// ? data[midd+1] : [];
     let lD = data.slice(0, midd);// ? data[midd-1] : [];
-console.log(rD);
-console.log(lD);
+    
     // TODO: break array recursively 
     mergeSort(rD);
     mergeSort(lD);
@@ -26,7 +26,7 @@ console.log(lD);
         k+=1;
 
     }
-    // one side of the search is finished so go and copy the rest into the end of the array because this is a sorted array by recurtion
+    // TODO: Copy remaining to Data
     while(i<lD.length){
         data[k]=lD[i]; i+=1; k+=1;
     }
@@ -39,8 +39,8 @@ console.log(lD);
     
 }
 let data = [1,8,7,6,5,4,3,2,9,3,6,4,7,8,4,5,2,6,8,97,4,3];
-// console.log(data);
+console.log(data);
 data = mergeSort(data);
 console.log(data);
-// data.forEach(i=>console.log(i));
+
 
