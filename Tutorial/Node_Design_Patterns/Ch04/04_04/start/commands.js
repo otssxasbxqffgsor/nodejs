820fe7e6251634caa09d3bda2fb76bff262809e8
kian <kian@blackberry-462d.digital-dispatch.com>
2019-07-22 10:12:18 -0700

    adding command pattern, with command super class

--- Tutorial/Node_Design_Patterns/Ch04/04_04/start/commands.js
+++ Tutorial/Node_Design_Patterns/Ch04/04_04/start/commands.js
@@ -1,26 +1,32 @@
-var {writeFile} = require('fs');
+var { writeFile } = require('fs');
 var path = require('path');
-
-class ExitCommand{
-    
+var command = require('./command');
+class ExitCommand extends command {
+    constructor(){
+        super();
+    }
     get name(){
-        return 'exit ... bye!'
+        return 'exit command byte!!';
     }
     execute(){
         process.exit(0);
     }
 }
-class CreateCommand{
-    constructor(fileName, text){
+class CreateCommand extends command{
+    constructor( fileName, text){
+        super();
         this.fileName = fileName;
         this.body = text;
         this.fullPath = path.join(__dirname, fileName);
+        console.log(text + this.body);
     }
     get name(){
         return `create ${this.fileName}`;
     }
     execute(){
-        writeFile(this.fileName, this.body, f=>f);
+        writeFile(this.fullPath, this.body, f=>f);
     }
+
 }
-module.exports = {ExitCommand, CreateCommand};
+
+module.exports = { ExitCommand, CreateCommand };
