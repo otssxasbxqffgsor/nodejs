ac0eebcd1c1918305d9a7847a10f7b4a21ae07e2
Kian Davoudi Rad <kdavoudirad@digital-dispatch.com>
2019-08-01 11:58:01 -0700

    commite change

--- Tutorial/Pattern/Ch02/02_05/start/ShopperCloned.js
+++ Tutorial/Pattern/Ch02/02_05/start/ShopperCloned.js
@@ -2,11 +2,6 @@ var Shopper = require('./Shopper');
 
 class ShopperCloned{
     constructor(name){
-        // let shopper =  new Shopper(name).clone();
-        // shopper.add('camping knife');
-        // shopper.add('tent');
-        // shopper.add('backpack');
-        // shopper.add('map');
         this.name = name;
         this.shopper;
         
