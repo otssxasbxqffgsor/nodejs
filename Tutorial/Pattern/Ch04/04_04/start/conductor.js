92c4fbfbef9789c399d140e0eb3a5d8ef7d211df
kian <kian@yvr-tx-paulc.digital-dispatch.com>
2019-08-06 10:57:27 -0700

    missed executation command in conductor

--- Tutorial/Pattern/Ch04/04_04/start/conductor.js
+++ Tutorial/Pattern/Ch04/04_04/start/conductor.js
@@ -2,6 +2,7 @@ class Conductor{
  
     run(command){
         console.log(`Executing command : ${command.name}`);
+        command.execute()
     }
 }
 module.exports = new Conductor();
--- Tutorial/Pattern/Ch04/04_04/start/index.js
+++ Tutorial/Pattern/Ch04/04_04/start/index.js
@@ -1,29 +1,35 @@
 var conductor = require('./conductor');
-var {ExitCommand, CreateCommand} = require('./command');
-var {createInterface} = require('readline');
+var { ExitCommand, CreateCommand } = require('./commands');
+
+var { createInterface }  = require('readline');
 var rl = createInterface({
     input: process.stdin,
-    output:process.stdout
+    output: process.stdout
 });
 
-console.log(`Create <FileName> <text> | exit`);
+console.log('create <fileName> <text> | exit');
 rl.prompt();
 
-rl.on('on', input =>{
-    var [commandText, ...remaining ]= input.split(' ');
-    var [fileName, ...fileText] = remaining;
-    var text = fileText.join(' ');
+rl.on('line', input => {
 
-    switch(commandText){
-        case 'exit':
+    var [ commandText, ...remaining ] = input.split(' ')
+    var [ fileName, ...fileText ] = remaining
+    var text = fileText.join(' ')
+
+    switch(commandText) {
+
+        case "exit":
             conductor.run(new ExitCommand());
             break;
-        case 'create':
+
+        case "create" :
             conductor.run(new CreateCommand(fileName, text));
             break;
-        
-            default:
-                console.log(`${commandText} command not found`);
+
+        default :
+            console.log(`${commandText} command not found!`);
     }
+
     rl.prompt();
+
 });
